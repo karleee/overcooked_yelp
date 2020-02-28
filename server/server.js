0141362ce52fdde9dd149810d7db76d75e4b5835
karleee <karleee@protonmail.com>
2020-02-28 09:56:36 -0800

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('../config/keys.js').mongoURI;
+
+const app = express();
+
+if (!db) {
+  throw new Error("You must provide a string to connect to MongoDB Atlas");
+}
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+// remember we use bodyParser to parse requests into json
+app.use(bodyParser.json());
+
+// set up heroku deploy
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+// apollo setup
+let uri;
+
+if (process.env.NODE_ENV === "production") {
+  uri = `/graphql`;
+} else {
+  uri = "http://localhost:5000/graphql";
+}
+
+const httpLink = createHttpLink({
+  uri,
+  headers: {
+    authorization: localStorage.getItem("auth-token")
+  }
+});
+
+module.exports = app;
+
+// sudo lsof -i :5000
+
+// sudo kill -9 <PID> 
