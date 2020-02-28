2f36a3990859da69c38bbc8c8d2563b84811d262
karleee <karleee@protonmail.com>
2020-02-28 13:47:46 -0800

    Change double quotes

--- server/server.js
+++ server/server.js
@@ -21,20 +21,20 @@ mongoose
 app.use(bodyParser.json());
 
 // Setting up Heroku deploy 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
 // Setting up Apollo
 let uri;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   uri = `/graphql`;
 } else {
-  uri = "http://localhost:5000/graphql";
+  uri = 'http://localhost:5000/graphql';
 }
 
 // const httpLink = createHttpLink({
