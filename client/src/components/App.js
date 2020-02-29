bc0180f18faa8bcc85920866556a70fdcda40555
karleee <karleee@protonmail.com>
2020-02-28 21:55:33 -0800

    Add homepage styling

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from '../logo.svg';
-import '../App.css';
+import '../stylesheets/reset.css';
+import '../stylesheets/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+      <div className="banner-wrapper"></div>
+
+      <h1>Overcooked</h1>
+
+      <div className="search-input-wrapper">
+        <div className="find-input-wrapper">
+          <label>
+            Find
+            <input
+              type="text"
+              placeholder="burgers, pancakes, burritos, salads..."
+            />
+          </label>
+        </div>
+
+        <div className="separator-wrapper"></div>
+
+        <div className="near-input-wrapper">
+          <label>
+            Near
+            <input
+              type="text"
+              placeholder="Put user's current location here"
+            />
+          </label>
+        </div>
+
+        <div className="search-button-wrapper"></div>
+      </div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 // React setup
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
+
+// Apollo setup
+import ApolloClient from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+import { onError } from 'apollo-link-error';
 
 // Local file imports
 import './index.css';
@@ -9,48 +16,47 @@ import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 // Mapping objects to mongo _id to set up Apollo cache
-// const cache = new InMemoryCache({
-//   dataIdFromObject: object => object._id || null
-// });
+const cache = new InMemoryCache({
+  dataIdFromObject: object => object._id || null
+});
 
 // Using apollo-link-http library to carry data between backend and frontend server
 // Makes requests over HTTP to the graphql endpoint to get info from backend
 // Verifies user by sending auth token in the headers up with every request
-// const httpLink = createHttpLink({
-//   uri: 'http://localhost:5000/graphql',
-//   headers: {
-//     authorization: localStorage.getItem('auth-token')
-//   }
-// });
+const httpLink = createHttpLink({
+  uri: 'http://localhost:5000/graphql',
+  headers: {
+    authorization: localStorage.getItem('auth-token')
+  }
+});
 
 // Configuring onError from apollo-link-error library to log any errors received
 // from backend
-// const errorLink = onError(({ graphQLErrors }) => {
-//   if (graphQLErrors) graphQLErrors.map(({ message }) => console.log(message));
-// });
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors) graphQLErrors.map(({ message }) => console.log(message));
+});
 
 // Configuring client:
 // link – tells cache how to fetch data
 // cache – the cache we just configured before
 // onError – specifies how to handle errors
-// const client = new ApolloClient({
-//   link: httpLink,
-//   cache,
-//   onError: ({ networkError, graphQLErrors }) => {
-//     console.log('graphQLErrors', graphQLErrors);
-//     console.log('networkError', networkError);
-//   }
-// });
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log('graphQLErrors', graphQLErrors);
+    console.log('networkError', networkError);
+  }
+});
 
 // Creating a Root component to wrap the application in
 const Root = () => {
   return (
-    // <ApolloProvider client={client}>
-    //   <HashRouter>
-    //     <App />
-    //   </HashRouter>
-    // </ApolloProvider>
-    <h1>Success</h1>
+    <ApolloProvider client={client}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ApolloProvider>
   );
 }
 
