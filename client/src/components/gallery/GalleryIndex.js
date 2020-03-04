193163ee475d4ea5cd75625f13d32cab53a794ef
karleee <karleee@protonmail.com>
2020-03-03 21:19:48 -0800

    Add modal styling

--- client/src/components/gallery/GalleryIndex.js
+++ client/src/components/gallery/GalleryIndex.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import { Link } from 'react-router-dom';
 import Queries from '../../graphql/queries';
 import Navbar from '../navbar/Navbar';
+import Modal from '../modal/Modal';
 import '../../assets/stylesheets/reset.css';
 import '../../assets/stylesheets/App.css';
 import '../../assets/stylesheets/GalleryIndex.css';
@@ -10,104 +11,129 @@ import '../../assets/stylesheets/GalleryIndex.css';
 const { FETCH_RESTAURANT } = Queries; 
 
 // GalleryIndex component returning photo gallery of images from one restaurant from backend
-const GalleryIndex = props => {
-  return (
-    <Query query={FETCH_RESTAURANT} variables={{ _id: props.match.params.id }}>
-      {({ loading, error, data }) => {
-        if (loading) return 'Loading...';
-        if (error) return `Error! ${error.message}`;
+class GalleryIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      showModal: false,
+      currentImage: 0 
+    }
+    this.toggleModal = this.toggleModal.bind(this);
+  }
 
-        // Saving restaurant reviews to a variable for easier access
-        const reviews = data.restaurant.reviews;
+  // Toggles the gallery modal on and off
+  toggleModal(indx) {
+    this.setState({ 
+      showModal: !this.state.showModal,
+      currentImage: indx
+    });
+  }
+  
+  // Renders the component
+  render() {
+    return (
+      <Query query={FETCH_RESTAURANT} variables={{ _id: this.props.match.params.id }}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error.message}`;
 
-        // Getting average rating from all reviews
-        let total = 0;
-        let average;
+          // Saving restaurant reviews to a variable for easier access
+          const reviews = data.restaurant.reviews;
 
-        reviews.forEach(review => {
-          total += review.rating;
-        });
+          // Getting average rating from all reviews
+          let total = 0;
+          let average;
 
-        average = total / reviews.length;
+          reviews.forEach(review => {
+            total += review.rating;
+          });
 
-        // Determining star ratings indicator
-        let stars;
+          average = total / reviews.length;
 
-        if (average === 0) {
-          stars = 'zero';
-        } else if (average > 0 && average <= 1) {
-          stars = 'one';
-        } else if (average > 1 && average < 1.6) {
-          stars = 'one_and_half';
-        } else if (average >= 1.6 && average <= 2) {
-          stars = 'two';
-        } else if (average > 2 && average < 2.6) {
-          stars = 'two_and_half';
-        } else if (average >= 2.6 && average <= 3) {
-          stars = 'three';
-        } else if (average > 3 && average < 3.6) {
-          stars = 'three_and_half';
-        } else if (average >= 3.6 && average <= 4) {
-          stars = 'four';
-        } else if (average > 4 && average < 4.6) {
-          stars = 'four_and_half';
-        } else {
-          stars = 'five';
-        }
+          // Determining star ratings indicator
+          let stars;
 
-        return (
-          <div className="restaurant-photo-gallery-wrapper">      
-            <Navbar />
+          if (average === 0) {
+            stars = 'zero';
+          } else if (average > 0 && average <= 1) {
+            stars = 'one';
+          } else if (average > 1 && average < 1.6) {
+            stars = 'one_and_half';
+          } else if (average >= 1.6 && average <= 2) {
+            stars = 'two';
+          } else if (average > 2 && average < 2.6) {
+            stars = 'two_and_half';
+          } else if (average >= 2.6 && average <= 3) {
+            stars = 'three';
+          } else if (average > 3 && average < 3.6) {
+            stars = 'three_and_half';
+          } else if (average >= 3.6 && average <= 4) {
+            stars = 'four';
+          } else if (average > 4 && average < 4.6) {
+            stars = 'four_and_half';
+          } else {
+            stars = 'five';
+          }
+          // console.log(this.state.showModal);
 
-            <div className="restaurant-photo-gallery-main-content-wrapper">
-              <div className="main-content-wrapper">
-                <h1>Photos for {data.restaurant.name}</h1>
+          return (
+            <div className="restaurant-photo-gallery-wrapper">      
+              <Navbar />
 
-                <div className="photo-gallery-header">
-                  <div className="photo-gallery-thumbnail-wrapper">
-                    <img src={data.restaurant.photos[0]} alt="Restaurant thumbnail image" />
-                  </div>
+              <div className="restaurant-photo-gallery-main-content-wrapper">
+                <div className="main-content-wrapper">
+                  <h1>Photos for {data.restaurant.name}</h1>
 
-                  <div className="photo-gallery-text-wrapper">
-                    <Link to={`/restaurants/${data.restaurant._id}`}>{data.restaurant.name}</Link>
+                  <div className="photo-gallery-header">
+                    <div className="photo-gallery-thumbnail-wrapper">
+                      <img src={data.restaurant.photos[0]} alt="Restaurant thumbnail image" />
+                    </div>
 
-                    <div className="photo-gallery-stars-icon-wrapper">
-                      <img src={`/images/restaurant_detail/${stars}.png`} />
+                    <div className="photo-gallery-text-wrapper">
+                      <Link to={`/restaurants/${data.restaurant._id}`}>{data.restaurant.name}</Link>
+
+                      <div className="photo-gallery-stars-icon-wrapper">
+                        <img src={`/images/restaurant_detail/${stars}.png`} />
+                      </div>
+                    </div>
+
+                    <div className="photo-gallery-reviews-wrapper">
+                      <p>{reviews.length} {reviews.length > 1 || reviews.length === 0 ? 'reviews' : 'review'}</p>
                     </div>
                   </div>
 
-                  <div className="photo-gallery-reviews-wrapper">
-                    <p>{reviews.length} {reviews.length > 1 || reviews.length === 0 ? 'reviews' : 'review'}</p>
-                  </div>
-                </div>
-
-                <div className="photo-gallery-filter-options-wrapper">
-                  <div className="photo-gallery-filter-links-wrapper">
-                    <p>All</p>
-                    <p>Food</p>
-                    <p>Inside</p>
-                    <p>Drink</p>
-                    <p>Menu</p>
-                    <p>Outside</p>
-                  </div>
-                  
-                  <div className="photo-gallery-underline-wrapper"></div>
-                </div>
-
-                <div className="photo-gallery-main-gallery-wrapper">
-                  {data.restaurant.photos.map(photo => 
-                    <div className="gallery-thumbnail-photo-wrapper">
-                      <img src={photo} alt="Restaurant photo thumbnail" />
+                  <div className="photo-gallery-filter-options-wrapper">
+                    <div className="photo-gallery-filter-links-wrapper">
+                      <p>All</p>
+                      <p>Food</p>
+                      <p>Inside</p>
+                      <p>Drink</p>
+                      <p>Menu</p>
+                      <p>Outside</p>
                     </div>
-                  )}
+                    
+                    <div className="photo-gallery-underline-wrapper"></div>
+                  </div>
+
+                  <div className="photo-gallery-main-gallery-wrapper">
+                    {data.restaurant.photos.map((photo, indx) => 
+                      <div key={indx} className="gallery-thumbnail-photo-wrapper" onClick={() => this.toggleModal(indx)}>
+                        <img src={photo} alt="Restaurant photo thumbnail" />
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
+
+              <div className="photo-gallery-modal-wrapper" onClick={this.toggleModal}>
+                {this.state.showModal ? <Modal restaurant={data.restaurant} imageNum={this.state.currentImage} /> : ''}
+              </div>
             </div>
-          </div>
-        );
-      }}
-    </Query>
-  );
+          );
+        }}
+      </Query>
+    );
+  }
 };
 
 export default GalleryIndex;
---
+++ client/src/components/modal/Modal.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import '../../assets/stylesheets/Modal.css';
+
+class Modal extends React.Component {
+  // Constructor for Modal
+  constructor(props) {
+    super(props);
+    this.state = { 
+      currentImage: this.props.imageNum,
+      allImages: this.props.restaurant.photos
+    };
+    this.previous = this.previous.bind(this);
+    this.next = this.next.bind(this);
+  }
+
+  // Hides scrolling when modal is mounted
+  componentDidMount() {
+    if (this.props.modal) document.body.style.overflow = 'hidden';
+  }
+
+  // Reactiviates scrolling when modal is unmounted
+  componentWillUnmount() {
+    document.body.style.overflow = 'unset';
+  }
+
+  // Handles image clicking
+  handleClick(num) {
+    this.setState({ currentImage: num });
+  }
+
+  // Goes to previous photo
+  previous() {
+    let newImage = ((this.state.currentImage - 1) + this.state.allImages.length) % this.props.allImages.length;
+    this.setState({ currentImage: newImage });
+  }
+
+  // Goes to next photo
+  next() {
+    let newImage = (this.state.currentImage + 1) % this.props.allImages.length;
+    this.setState({ currentImage: newImage });
+  }
+
+  // Renders component
+  render() {
+    return (
+      <div className="modal-wrapper">  
+        <div className="modal-main-content-wrapper">    
+          <div className="main-image-wrapper">
+            <div className="previous-arrow-wrapper" onClick={this.previous}>
+              <i className="previous-arrow"></i>
+            </div>
+
+            <div className="selected-image-wrapper">
+              <img src={this.state.allImages[this.state.currentImage]} alt="Gallery photo" />
+            </div>
+
+            <div className="next-arrow-wrapper" onClick={this.next}>
+              <i className="next-arrow"></i>
+            </div>
+          </div>
+
+          <div className="modal-sidebar-wrapper">
+            <p>Put user info here</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
