4c37134d341ac279db8231c7d630b9bc10327df3
karleee <karleee@protonmail.com>
2020-03-19 14:08:43 -0700

    Remove extra comments

--- client/src/components/modal/Modal.js
+++ client/src/components/modal/Modal.js
@@ -32,8 +32,6 @@ class Modal extends React.Component {
 
   // Renders component
   render() {
-    // console.log(this.state.allPhotos[this.state.currentPhoto]);
-    // console.log(this.state.allPhotos);
     const currentPhoto = this.state.allPhotos[this.state.currentPhoto];
     const date = currentPhoto.review.date; 
 
