2017551f52181542b7222ef5af2cdd101fd769a3
karleee <karleee@protonmail.com>
2020-04-01 12:43:42 -0700

    Finish three star pop up box

--- client/src/components/reviews/ReviewFirst.js
+++ client/src/components/reviews/ReviewFirst.js
@@ -61,10 +61,29 @@ const ReviewFirst = () => {
                               </div>
                             </div>
 
-                            <div className="two-star-wrapper"></div>
-                            <div className="three-star-wrapper"></div>
-                            <div className="four-star-wrapper"></div>
-                            <div className="five-star-wrapper"></div>
+                            <div className="two-star-wrapper">
+                              <div className="two-star-popup-wrapper">
+                                <p>Meh. I've experienced better.</p>
+                              </div>
+                            </div>
+
+                            <div className="three-star-wrapper">
+                              <div className="three-star-popup-wrapper">
+                                <p>A-OK.</p>
+                              </div>
+                            </div>
+
+                            <div className="four-star-wrapper">
+                              <div className="four-star-popup-wrapper">
+                                <p>Yay! I'm a fan.</p>
+                              </div>
+                            </div>
+
+                            <div className="five-star-wrapper">
+                              <div className="five-star-popup-wrapper">
+                                <p>Woohoo! As good as it gets!</p>
+                              </div>
+                            </div>
 
                             <i className="rating-image"></i>
                           </Link>
